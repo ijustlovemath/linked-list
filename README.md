d6af4aa955445a2b2c4b66bc317df4aaaa8cc0ba
Jeremy DeJournett <jcdejournett@gmail.com>
2021-11-26 22:56:49 -0500

    Added a lot more documentation to the implementation
    Explained the reversal algorithm and some stylistic choices, as well
    as made some minor cosmetic clarifications within the code to make
    it more teachable.

--- README.md
+++ README.md
@@ -13,3 +13,11 @@ There's also a simple GitHub Action workflow file included, which will automatic
 To add new functionality, simply edit ll.h with your prototypes, and ll.c with your implementations. Then, to add tests, write test code in `tests.c` and execute it with `make test`
 
 A simple vim session is provided, and can be used to edit `ll.c, ll.h, and tests.c` by running `vim -S ll.vim`
+
+## Features
+
+- a simple implementation that prioritizes readability over terseness
+- a three-pointer iterative approach to reversing the linked list
+- minimal but complete environment, with separation of implementation and tests
+- fully stack-based memory management, so you don't have to worry about debugging memory allocations
+- built to be able to use heap memory if that's something you want to practice
--- ll.c
+++ ll.c
@@ -3,9 +3,41 @@
 
 #include "ll.h"
 
-void print_node(const struct singly_linked *root) {
-    if(root) {
-        printf("%p -> %d\n", root, root->value);
+
+/**
+ * An aside about if statements and while loops:
+ *
+ * Several places in this file you'll see pointers
+ * used in if statements, like:
+ *
+ * if(root)
+ *
+ * OR
+ *
+ * while(root)
+ *
+ * This is a shorthand notation in C for
+ *
+ * if(root != NULL)
+ *
+ * AND
+ *
+ * while(root != NULL)
+ *
+ * The reason why this works is because, in C, pointers
+ * can be thought of as numerical addresses. These addresses
+ * are always greater than 0, and in C, if(some_number)
+ * is the _exact same_ as if(some_number != 0).
+ *
+ * Addresses are usually large numbers divisible by 8 or 16 or 32 or 64, like 139502496.
+ *
+ * This is because, for better or worse, 0 is "False" in C,
+ * and everything else is "True", even negative numbers like -1.
+ */
+
+void print_node(const struct singly_linked *node) {
+    if(node) { 
+        printf("%p -> %d\n", node, node->value);
     } else {
         puts("NULL");
     }
@@ -31,6 +63,12 @@ void init_node(struct singly_linked *new, int value) {
 int insert_node(struct singly_linked *root, struct singly_linked *new) {
 
     /**
+     * Insert the node "new" right after the node "root".
+     * If "root" had a node after it, then "new" will now
+     * have that same node after it, and "root" will have
+     * "new" after it.
+     *
+     *
      * 1. set new's next to whatever roots next is
      * 2. set root's next to new
      * 3. 1 for success, 0 for failure
@@ -45,6 +83,22 @@ int insert_node(struct singly_linked *root, struct singly_linked *new) {
 }
 
 struct singly_linked* create_ordered_list(int total_nodes) {
+    // If we use a static array here, we can
+    // return addresses in this array and they will
+    // still be valid after this function returns!
+    //
+    // This is an easy way to use stack based memory,
+    // but with the ergonomics and ease-of-use of the
+    // heap.
+    //
+    // This approach does not work when:
+    // 1) the code is multi-threaded
+    // 2) you need many thousands or millions of nodes
+    // in your list. 
+    //
+    // Because this code is for teaching, and memory
+    // management can be a pain, we will use this approach
+    // and make our lives easier.
     static struct singly_linked nodes[100];
 
     if(total_nodes < 0 || total_nodes > 100) {
@@ -52,16 +106,26 @@ struct singly_linked* create_ordered_list(int total_nodes) {
         return NULL;
     }
 
-    // Degenerate but valid case
+    // Degenerate but valid case; not an error
     if(total_nodes == 0) {
         return NULL;
     }
 
     // Clear them out; this *automagically* sets all next pointers to NULL!
+    // TODO: only clear out nodes we're going to use
     memset(nodes, 0, sizeof(nodes));
 
+    // For each node requested, set its data equal to
+    // its own index within our static array.
+    //
+    // This choice doesn't mean anything, you can also use
+    // random numbers if you like, but this is an easier
+    // pattern to see if things are working
     for(int i = 0; i < total_nodes; i++) {
         init_node(nodes + i, i);
+
+        // Skip the very first node, which has no predecessor
+        // for us to insert it after
         if(i) {
             insert_node(nodes + i - 1, nodes + i);
         }
@@ -80,20 +144,52 @@ void reverse_list(struct singly_linked **root) {
      * initial:
      * a -> b -> c -> d
      * 
+     * At the beginning, previous is undefined, so use NULL (0)
+     * Current (*) is the head of the list, aka a
+     * Next (^) is the node following head 
      * p    *    ^
-     * a -> b -> c -> d
+     * 0    a -> b -> c -> d
      *
+     * Start by severing the link between a and b,
+     * overwriting it by making a point to NULL instead
+     *
+     * This is okay, because we have ^, which allows us
+     * to continue iterating through the list without
+     * losing it! This situation is why this approach
+     * needs 3 pointers total.
+     *
+     * The three pointers function like a rolling bridge,
+     * allowing us to cross 'gaps' in the data structure
+     * without losing access to either side!
      * p    *    ^
+     * 0 <- a    b -> c -> d
+     *
+     *
+     * Once we've reassigned the first link,
+     * we just move the 'pointer bridge' over by 1,
+     * and start over, doing the same thing:
+     *
+     * Starting arrangement:
+     *      p    *    ^
+     * 0 <- a    b -> c -> d
+     
+     * 1. Making the current node point to the previous node
+     *      p    *    ^
      * 0 <- a <- b    c -> d
      *
-     *      p    *    ^
-     * a <- b    c -> d
+     * 2. Sliding the bridge along!
+     *           p    *    ^
+     * 0 <- a <- b -> c -> d
      *
      */
+
+    // At the very beginning, we check to see if the
+    // root node is itself NULL, in which case our
+    // work here is done!
     if(!root || !*root) {
         return;
     }
-    struct singly_linked *current=*root, *previous=NULL, *next=(*root)->next;
+    struct singly_linked *current=*root, *previous=NULL, *next=current->next;
 
 #if 0
 #define DBG_PRINT(name_) printf("%s: ", #name_); print_node(name_);
@@ -105,27 +201,45 @@ void reverse_list(struct singly_linked **root) {
         DBG_PRINT(current)
         DBG_PRINT(previous)
         DBG_PRINT(next)
+        // Reassign the link
         // p    *   ^        
         // a <- b   c -> d
         current->next = previous;
 
+        // Slide the previous part of the bridge
+        // At this point, both previous and current
+        // point to 'b'
         //      p*  ^ 
         // a <- b   c -> d
         previous = current;
 
+        // Slide the current part of the bridge
+        // At this point, only previous points to 'b'
+        // Both current and next point to 'c'
         //      p   *^
         // a <- b   c -> d
         current = next;
 
+        // Complete the bridge movement by making next
+        // point to 'd'.
         //      p   *    ^
         // a <- b   c -> d
         next = next->next;
+
+        // Then we do it all over again!
     };
 
     // At the very end, we've disconnected the new root from the rest,
     // so connect it back to everything else!
     // TODO: there's probably a way to reorder the steps to remove
     // the need to do this
+    // In essence, at the end of the previous loop,
+    // the data structure looks like this:
+    //                p    *    ^
+    // 0 <- a <- b <- c    d -> 0
+    //
+    // So this last step connects d to c, and
+    // reassigns d to be the new head of the list
     current->next = previous;
 
     *root = current;
